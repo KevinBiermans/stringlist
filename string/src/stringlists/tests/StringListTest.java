231e03d7fc291079595f8416926ddc01256f7181
kevin <kevin@192.168.10.101>
2021-02-18 13:56:13 +0100

    idk doei

--- string/src/stringlists/tests/StringListTest.java
+++ string/src/stringlists/tests/StringListTest.java
@@ -4,11 +4,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import stringlists.StringList;
+
 class StringListTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		String[] myStrings = {"Hello", "Bye"};
+		StringList myStringList = new StringList(myStrings);
+		
+		/*myStrings[0] = null;
+				
+		assertThrows(NullPointerException.class, () ->
+				myStringList.getConcatenation()); */
+		//gooit een NPE als we geen .clone() gebruiken bij this.elements = elements.clone()
+		
+		
+		String[] yourStrings = myStringList.getElements();
+		
+		yourStrings[0] = null;
+				
+		assertThrows(NullPointerException.class, () ->
+				myStringList.getConcatenation());
+		// gooit een NPE als we geen .clone() gebruiken bij getElements()
 	}
 
 }
